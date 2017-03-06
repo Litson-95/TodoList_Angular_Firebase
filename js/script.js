0e19204bb4bcdab531270458f064aaa78db1f3f2
Litson-95 <suriyagraphics123@gmail.com>
2017-03-06 22:06:22 +0530

    searchBar modified,added date

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 var app = angular.module("myApp", ["firebase"]);
 
 app.controller("myCtrl", function($scope, $firebaseArray) {
-    var ref = firebase.database().ref().child('todo/').limitToLast(20);
+    var ref = firebase.database().ref().child('todo/').orderByChild("sortDate");
     // download the data into a local object
     $scope.posts = $firebaseArray(ref);
+    console.log($scope.posts.length);
+
+    $scope.deletePost = function(list){
+        $scope.posts.$remove(list);
+    }
+
+    var addItems;
 
 });
