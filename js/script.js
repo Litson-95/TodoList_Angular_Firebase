0131b7c2b6d4aa2567610f8fd13622cd7ea01c30
Litson-95 <suriyagraphics123@gmail.com>
2017-03-06 13:26:16 +0530

    deletePost() in Script Js

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ app.controller("myCtrl", function($scope, $firebaseArray) {
     var ref = firebase.database().ref().child('todo/').orderByChild("sortDate");
     // download the data into a local object
     $scope.posts = $firebaseArray(ref);
-
+    $scope.deletePost = function(list){
+        $scope.posts.$remove(list);
+    }
 });
