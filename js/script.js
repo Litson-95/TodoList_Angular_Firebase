9f92452e9ff0624d7d1dbc082ccc52f9a1de0ac1
Litson-95 <suriyagraphics123@gmail.com>
2017-03-06 22:22:33 +0530

    added date()

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ app.controller("myCtrl", function($scope, $firebaseArray) {
     var ref = firebase.database().ref().child('todo/').orderByChild("sortDate");
     // download the data into a local object
     $scope.posts = $firebaseArray(ref);
+    console.log($scope.posts.length);
+
     $scope.deletePost = function(list){
         $scope.posts.$remove(list);
     }
+
+    var addItems;
+
 });
